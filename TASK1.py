832955ec3bc2b25d000e4e2e4cca1d664c63318d
GopalSingh-Byte <gopalsingh.Biotech@gmail.com>
2025-05-24 12:07:35 +0530

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,6 @@
+num1 = 10
+num2 = 30
+print("\nAddition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
---
+++ TASK2.py
@@ -0,0 +1,5 @@
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+Full_name = First_name + " " + Last_name
+print(f"Hello, {Full_name}! Welcome to the Python program.")
+
